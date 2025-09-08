fc5a18e7306a9e30b96ce0993b36af7c2a90323b
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2025-09-08 11:03:11 +0200

    Klart!

--- src/App.java
+++ src/App.java
@@ -6,24 +6,26 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Random numberGenerator = new Random();
 
-        int randomNumber = numberGenerator.nextInt(10) + 1; // 1-10
+        int randomNumber = numberGenerator.nextInt(10) + 1;
         int försök = 3;
         boolean gissadeRätt = false;
 
         System.out.println("Du ska gissa ett tal mellan 1-10, du har 3 försök!");
 
         for (int i = 1; i <= försök; i++) {
-            System.out.print("Försök " + i + "Ange ett tal: ");
+            System.out.print("Försök " + i + ". Ange ett tal: ");
             int gissning = scanner.nextInt();
             if (gissning == randomNumber) {
                 System.out.println("Rätt gissat! Talet var " + randomNumber);
                 gissadeRätt = true;
-                break; // Avsluta loopen om man gissar rätt
+                break; 
             } else {
                 System.out.println("Fel gissat!");
             }
         }
-
+            if (!gissadeRätt) {
+            System.out.println("Du gissade inte rätt. Talet var " + randomNumber);
+        }
 
         scanner.close();
     }
